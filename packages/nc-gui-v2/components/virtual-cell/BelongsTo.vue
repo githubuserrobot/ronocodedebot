nocodb/nocodb

feat(gui-v2): LTAR form style correction
Signed-off-by: Pranav C <pranavxc@gmail.com>

packages/nc-gui-v2/components/virtual-cell/BelongsTo.vue
@@ -3,7 +3,7 @@ import type { ColumnType } from 'nocodb-sdk'
 import ItemChip from './components/ItemChip.vue'
 import ListItems from './components/ListItems.vue'
 import { useProvideLTARStore } from '#imports'
-import { CellValueInj, ColumnInj, ReloadViewDataHookInj, RowInj } from '~/context'
+import { CellValueInj, ColumnInj, IsFormInj, ReloadViewDataHookInj, RowInj } from '~/context'
 import MdiExpandIcon from '~icons/mdi/arrow-expand'
 
 const column = inject(ColumnInj)
@@ -13,6 +13,7 @@ const row = inject(RowInj)
 const active = false
 const localState = null
 const listItemsDlg = ref(false)
+const isForm = inject(IsFormInj)
 
 const { relatedTableMeta, loadRelatedTableMeta, relatedTablePrimaryValueProp, unlink } = useProvideLTARStore(
   column as Required<ColumnType>,
@@ -24,6 +25,7 @@ await loadRelatedTableMeta()
 
 <template>
   <div class="flex w-full chips-wrapper align-center" :class="{ active }">
+    <template v-if="!isForm">
     <div class="chips d-flex align-center flex-grow">
       <template v-if="cellValue || localState">
         <ItemChip :item="cellValue" :value="cellValue[relatedTablePrimaryValueProp]" @unlink="unlink(cellValue || localState)" />
@@ -35,7 +37,9 @@ await loadRelatedTableMeta()
         @click="listItemsDlg = true"
       />
     </div>
-    <ListItems v-model="listItemsDlg" />
+    </template>
+    <ListChildItems />
+    <ListItems v-model="listItemsDlg"  @attach-record=" (listItemsDlg = true)"/>
   </div>
 </template>
 

packages/nc-gui-v2/components/virtual-cell/ManyToMany.vue
@@ -4,7 +4,7 @@ import ItemChip from './components/ItemChip.vue'
 import ListChildItems from './components/ListChildItems.vue'
 import ListItems from './components/ListItems.vue'
 import { useProvideLTARStore } from '#imports'
-import { CellValueInj, ColumnInj, ReloadViewDataHookInj, RowInj } from '~/context'
+import { CellValueInj, ColumnInj, IsFormInj, ReloadViewDataHookInj, RowInj } from '~/context'
 import MdiExpandIcon from '~icons/mdi/arrow-expand'
 import MdiPlusIcon from '~icons/mdi/plus'
 
@@ -12,6 +12,7 @@ const column = inject(ColumnInj)
 const row = inject(RowInj)
 const cellValue = inject(CellValueInj)
 const reloadTrigger = inject(ReloadViewDataHookInj)
+const isForm = inject(IsFormInj)
 
 const listItemsDlg = ref(false)
 const childListDlg = ref(false)
@@ -27,6 +28,7 @@ await loadRelatedTableMeta()
 
 <template>
   <div class="flex align-center gap-1 w-full h-full chips-wrapper">
+    <template v-if="!isForm">
     <!--    <template v-if="!isForm"> -->
     <div class="chips flex align-center img-container flex-grow hm-items flex-nowrap min-w-0 overflow-hidden">
       <template v-if="cellValue">
@@ -39,6 +41,7 @@ await loadRelatedTableMeta()
       <MdiExpandIcon class="text-sm nc-action-icon text-gray-500/50 hover:text-gray-500" @click="childListDlg = true" />
       <MdiPlusIcon class="text-sm nc-action-icon text-gray-500/50 hover:text-gray-500" @click="listItemsDlg = true" />
     </div>
+    </template>
     <ListItems v-model="listItemsDlg" />
     <ListChildItems v-model="childListDlg" @attach-record=";(childListDlg = false), (listItemsDlg = true)" />
   </div>

packages/nc-gui-v2/components/virtual-cell/components/ListChildItems.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { watchEffect } from '@vue/runtime-core'
 import { Modal } from 'ant-design-vue'
 import { useLTARStoreOrThrow, useVModel } from '#imports'
 import { IsFormInj } from '~/context'
@@ -23,9 +24,9 @@ const {
   getRelatedTableRowId,
 } = useLTARStoreOrThrow()
 
-watch([vModel, isForm], () => {
+watchEffect(() => {
   if (vModel.value || isForm) {
-    loadChildrenList()
+    loadChildrenList?.()
   }
 })
 
@@ -48,7 +49,7 @@ const container = computed(() =>
       <div class="flex mb-4 align-center gap-2">
         <!-- <a-input v-model:value="childrenListPagination.query" class="max-w-[200px]" size="small"></a-input> -->
         <div class="flex-1" />
-        <MdiReloadIcon class="cursor-pointer text-gray-500" @click="loadChildrenList" />
+        <MdiReloadIcon v-if="!isForm" class="cursor-pointer text-gray-500" @click="loadChildrenList" />
         <a-button type="primary" size="small" @click="emit('attachRecord')">
           <div class="flex align-center gap-1">
             <MdiUnlinkIcon class="text-xs text-white" @click="unlinkRow(row)" />

githubuserrobot/ronocodedebot

packages/nc-gui-v2/components/virtual-cell/BelongsTo.vue
@@ -4,7 +4,7 @@ import type { Ref } from 'vue'
 import ItemChip from './components/ItemChip.vue'
 import ListItems from './components/ListItems.vue'
 import { inject, ref, useProvideLTARStore } from '#imports'
-import { CellValueInj, ColumnInj, ReloadViewDataHookInj, RowInj } from '~/context'
+import { CellValueInj,IsFormInj, ColumnInj, ReloadViewDataHookInj, RowInj } from '~/context'
 
 const column = inject(ColumnInj)
 
@@ -18,7 +18,9 @@ const active = false
 
 const listItemsDlg = ref(false)
 
-const { loadRelatedTableMeta, relatedTablePrimaryValueProp, unlink } = useProvideLTARStore(
+const isForm = inject(IsFormInj)
+
+const { relatedTableMeta, loadRelatedTableMeta, relatedTablePrimaryValueProp, unlink } = useProvideLTARStore(
   column as Ref<Required<ColumnType>>,
   row,
   reloadTrigger.trigger,
@@ -29,6 +31,7 @@ await loadRelatedTableMeta()
 
 <template>
   <div class="flex w-full chips-wrapper align-center" :class="{ active }">
+    <template v-if="!isForm">
     <div class="chips d-flex align-center flex-grow">
       <template v-if="cellValue">
         <ItemChip :item="cellValue" :value="cellValue[relatedTablePrimaryValueProp]" @unlink="unlink(cellValue)" />
@@ -42,6 +45,9 @@ await loadRelatedTableMeta()
     </div>
 
     <ListItems v-model="listItemsDlg" />
+    </template>
+    <ListChildItems />
+    <ListItems v-model="listItemsDlg"  @attach-record="listItemsDlg = true"/>
   </div>
 </template>
 

packages/nc-gui-v2/components/virtual-cell/ManyToMany.vue
@@ -5,7 +5,7 @@ import ItemChip from './components/ItemChip.vue'
 import ListChildItems from './components/ListChildItems.vue'
 import ListItems from './components/ListItems.vue'
 import { computed, inject, ref, useProvideLTARStore } from '#imports'
-import { CellValueInj, ColumnInj, ReloadViewDataHookInj, RowInj } from '~/context'
+import { CellValueInj, ColumnInj, ReloadViewDataHookInj,IsFormInj, RowInj } from '~/context'
 
 const column = inject(ColumnInj)!
 
@@ -14,6 +14,8 @@ const row = inject(RowInj)!
 const cellValue = inject(CellValueInj)!
 
 const reloadTrigger = inject(ReloadViewDataHookInj)!
+
+const isForm = inject(IsFormInj)
 
 const listItemsDlg = ref(false)
 
@@ -42,6 +44,7 @@ const cells = computed(() =>
 
 <template>
   <div class="flex align-center gap-1 w-full h-full chips-wrapper">
+    <template v-if="!isForm">
     <div class="chips flex align-center img-container flex-grow hm-items flex-nowrap min-w-0 overflow-hidden">
       <template v-if="cellValue">
         <ItemChip v-for="(cell, i) of cells" :key="i" :value="cell.value" @unlink="unlink(cell.item)" />
@@ -55,6 +58,7 @@ const cells = computed(() =>
 
       <MdiPlus class="text-sm nc-action-icon text-gray-500/50 hover:text-gray-500" @click="listItemsDlg = true" />
     </div>
+    </template>
 
     <ListItems v-model="listItemsDlg" />
 

packages/nc-gui-v2/components/virtual-cell/components/ListChildItems.vue
@@ -1,4 +1,7 @@
 <script lang="ts" setup>
+import { watchEffect } from '@vue/runtime-core'
+import { Modal } from 'ant-design-vue'
+import { IsFormInj } from '~/context'
 import { useLTARStoreOrThrow, useVModel, watch } from '#imports'
 
 const props = defineProps<{ modelValue?: boolean }>()
@@ -44,7 +47,7 @@ const container = computed(() =>
       <div class="flex mb-4 align-center gap-2">
         <div class="flex-1" />
 
-        <MdiReload class="cursor-pointer text-gray-500" @click="loadChildrenList" />
+        <MdiReload  v-if="!isForm" class="cursor-pointer text-gray-500" @click="loadChildrenList" />
 
         <a-button type="primary" size="small" @click="emit('attachRecord')">
           <div class="flex align-center gap-1">
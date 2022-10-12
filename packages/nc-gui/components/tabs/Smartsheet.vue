harisishar/nocodb

wip(gui-v2): keep tab level view state(filter/sort/fields)
Signed-off-by: Pranav C <pranavxc@gmail.com>

packages/nc-gui-v2/components/tabs/Smartsheet.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { ColumnType, TableType } from 'nocodb-sdk'
+import type { ColumnType, TableType, ViewType } from 'nocodb-sdk'
 import type { Ref } from 'vue'
 import SmartsheetGrid from '../smartsheet/Grid.vue'
 import {
@@ -38,7 +38,22 @@ const meta = computed<TableType>(() => metas.value?.[tabMeta?.value?.id as strin
 const reloadEventHook = createEventHook<void>()
 const openNewRecordFormHook = createEventHook<void>()
 
-const { isGallery, isGrid, isForm, isLocked } = useProvideSmartsheetStore(activeView as Ref<TableType>, meta)
+const {
+  isGallery,
+  isGrid,
+  isForm,
+  isLocked,
+  nestedFilters,
+} = useProvideSmartsheetStore(activeView as Ref<TableType>, meta)
+
+watch(nestedFilters, (newFilters) => {
+  tabMeta.value.state = tabMeta.value.state || {}
+  tabMeta.value.state[activeView.value.id] = newFilters
+})
+
+watch(activeView, (newView: ViewType) => {
+  nestedFilters.value = tabMeta.value.state?.[newView.id!]
+})
 
 // provide the sidebar injection state
 provideSidebar({ storageKey: 'nc-right-sidebar' })

githubuserrobot/ronocodedebot

packages/nc-gui/components/tabs/Smartsheet.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { ColumnType, TableType } from 'nocodb-sdk'
+import type { ColumnType, TableType, ViewType } from 'nocodb-sdk'
 import {
   ActiveViewInj,
   FieldsInj,
@@ -37,7 +37,7 @@ const meta = computed<TableType | undefined>(() => activeTab.value && metas.valu
 
 const reloadEventHook = createEventHook()
 
-const { isGallery, isGrid, isForm, isKanban, isLocked } = useProvideSmartsheetStore(activeView, meta)
+const { isGallery, isGrid, isForm, isKanban, isLocked, nestedFilters } = useProvideSmartsheetStore(activeView, meta)
 
 const openNewRecordFormHook = createEventHook()
 
@@ -55,6 +55,15 @@ provide(OpenNewRecordFormHookInj, openNewRecordFormHook)
 provide(FieldsInj, fields)
 provide(IsFormInj, isForm)
 provide(TabMetaInj, activeTab)
+
+watch(nestedFilters, (newFilters) => {
+  activeTab.value.state = activeTab.value.state || {}
+  activeTab.value.state[activeView.value.id] = newFilters
+})
+
+watch(activeView, (newView: ViewType) => {
+  nestedFilters.value = activeTab.value.state?.[newView.id!]
+})
 </script>
 
 <template>
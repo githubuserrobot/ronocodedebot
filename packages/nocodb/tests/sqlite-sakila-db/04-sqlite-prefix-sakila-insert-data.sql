779a128fedc4b2b523288cbf265780aa0a3714e7
Muhammed Mustafa <mustafa010199@gmail.com>
2022-10-17 18:01:24 +0530

    feat(testing): Improved sqlite support

--- packages/nocodb/tests/sqlite-sakila-db/04-sqlite-prefix-sakila-insert-data.sql
+++ packages/nocodb/tests/sqlite-sakila-db/04-sqlite-prefix-sakila-insert-data.sql
@@ -1,17 +1,4 @@
-/*
 
-Sakila for SQLite is a port of the Sakila example database available for MySQL, which was originally developed by Mike Hillyer of the MySQL AB documentation team. 
-This project is designed to help database administrators to decide which database to use for development of new products
-The user can run the same SQL against different kind of databases and compare the performance
-
-License: BSD
-Copyright DB Software Laboratory
-http://www.etl-tools.com
-
-*/
-
-
--- Delete data
 DELETE FROM prefix___payment 
 ;
 DELETE FROM prefix___rental 

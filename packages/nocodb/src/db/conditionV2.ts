349526c54dcff3d0923fa7c735bbee13398f45a3
TheRushingWookie <quinnjarr@gmail.com>
2024-08-17 21:50:25 +0000

    Filter using LinkToAnotherRecord path as it works

--- packages/nocodb/src/db/conditionV2.ts
+++ packages/nocodb/src/db/conditionV2.ts
@@ -189,7 +189,8 @@ const parseConditionV2 = async (
         NcError.fieldNotFound(filter.fk_column_id);
       }
     }
-    if (column.uidt === UITypes.LinkToAnotherRecord) {
+
+    if (column.uidt === UITypes.LinkToAnotherRecord || column.uidt == UITypes.Links) {
       const colOptions = (await column.getColOptions(
         context,
       )) as LinkToAnotherRecordColumn;

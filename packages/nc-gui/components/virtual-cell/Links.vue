273558f8de7c9f0a92b91905b7de63c09f5861eb
TheRushingWookie <quinnjarr@gmail.com>
2024-08-17 22:15:20 +0000

    Add start:prod and disable trace GC

--- packages/nc-gui/components/virtual-cell/Links.vue
+++ packages/nc-gui/components/virtual-cell/Links.vue
@@ -90,7 +90,7 @@ const toatlRecordsLinked = computed(() => {
   if (isForm?.value) {
     return state.value?.[colTitle.value]?.length
   }
-  return +value?.value || 0
+  return +value?.value || ""
 })
 
 const onAttachRecord = () => {

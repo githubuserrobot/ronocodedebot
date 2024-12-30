dcc97159c3fd01cb041b2387f56144b608c0be6d
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-12-30 17:40:57 +0000

    fix: lint

--- packages/nocodb/src/db/BaseModelSqlv2.ts
+++ packages/nocodb/src/db/BaseModelSqlv2.ts
@@ -4941,7 +4941,7 @@ class BaseModelSqlv2 {
   async moveRecord({
     rowId,
     beforeRowId,
-    cookie,
+    _cookie,
   }: {
     rowId: string;
     beforeRowId: string;
@@ -7053,7 +7053,10 @@ class BaseModelSqlv2 {
           );
         }
 
-        if (column.system && ![UITypes.ForeignKey, UITypes.Order].includes(column.uidt) ) {
+        if (
+          column.system &&
+          ![UITypes.ForeignKey, UITypes.Order].includes(column.uidt)
+        ) {
           NcError.badRequest(
             `Column "${column.title}" is system column and cannot be updated`,
           );

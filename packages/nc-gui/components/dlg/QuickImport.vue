1eae2b2f11191c87e468e28745db3c51acb37459
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-01-10 16:11:08 +0000

    fix: rebase issues

--- packages/nc-gui/components/dlg/QuickImport.vue
+++ packages/nc-gui/components/dlg/QuickImport.vue
@@ -147,10 +147,6 @@ if (isWorkerSupport && process.env.NODE_ENV === 'production') {
   )
 }
 
-const collapseKey = ref('')
-const temporaryJson = ref({})
-const jsonErrorText = ref('')
-
 const isPreImportFileFilled = computed(() => {
   return importState.fileList?.length > 0
 })

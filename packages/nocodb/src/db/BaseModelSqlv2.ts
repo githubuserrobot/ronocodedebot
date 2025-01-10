e822a0adc31bbb1f47dfd9e380b1471f522c04fa
Pranav C <pranavxc@gmail.com>
2025-01-10 07:24:55 +0000

    fix: missing type def

--- packages/nocodb/src/db/BaseModelSqlv2.ts
+++ packages/nocodb/src/db/BaseModelSqlv2.ts
@@ -5462,6 +5462,7 @@ class BaseModelSqlv2 {
     nestedCols: Column[];
     data: Record<string, any>;
     insertObj: Record<string, any>;
+    req: NcRequest
   }) {
     const postInsertOps: ((rowId: any) => Promise<string>)[] = [];
     const preInsertOps: (() => Promise<string>)[] = [];
@@ -8778,7 +8779,7 @@ class BaseModelSqlv2 {
       model: auditConfig.parentModel,
       refModel: auditConfig.childModel,
       rowId,
-      childId,
+      refRowId: childId,
       columnTitle: auditConfig.parentColTitle,
       columnId: auditConfig.parentColId,
       req: cookie,

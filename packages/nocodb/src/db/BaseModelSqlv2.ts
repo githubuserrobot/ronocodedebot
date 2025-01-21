f135181380dd7ff1a4bf7fc0e7d106ea0efdb95a
Pranav C <pranavxc@gmail.com>
2025-01-21 09:50:08 +0000

    fix: map correct display value

--- packages/nocodb/src/db/BaseModelSqlv2.ts
+++ packages/nocodb/src/db/BaseModelSqlv2.ts
@@ -7968,7 +7968,7 @@ class BaseModelSqlv2 {
             : null;
 
           if (oldRowId) {
-            const [parentRelatedPkValue, _childRelatedPkValue] =
+            const [parentRelatedPkValue, childRelatedPkValue] =
               await this.readOnlyPrimariesByPkFromModel([
                 { model: childTable, id: childId },
                 { model: parentTable, id: oldRowId },
@@ -7982,7 +7982,8 @@ class BaseModelSqlv2 {
               opSubType: AuditOperationSubTypes.UNLINK_RECORD,
               columnTitle: auditConfig.parentColTitle,
               columnId: auditConfig.parentColId,
-              displayValue: parentRelatedPkValue,
+              refDisplayValue: parentRelatedPkValue,
+              displayValue: childRelatedPkValue,
               req: cookie,
               type: colOptions.type as RelationTypes,
             });
@@ -7996,7 +7997,8 @@ class BaseModelSqlv2 {
                 opSubType: AuditOperationSubTypes.UNLINK_RECORD,
                 columnTitle: auditConfig.childColTitle,
                 columnId: auditConfig.childColId,
-                refDisplayValue: parentRelatedPkValue,
+                displayValue: parentRelatedPkValue,
+                refDisplayValue: childRelatedPkValue,
                 req: cookie,
                 type: getOppositeRelationType(colOptions.type),
               });

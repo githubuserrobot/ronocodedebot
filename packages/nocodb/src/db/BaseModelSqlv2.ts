6185590cba967f260052b5af1b900cf801528286
Fendy Heryanto <fendy@nocodb.com>
2025-01-27 02:29:22 +0000

    remove console.log

--- packages/nocodb/src/db/BaseModelSqlv2.ts
+++ packages/nocodb/src/db/BaseModelSqlv2.ts
@@ -9458,8 +9458,6 @@ class BaseModelSqlv2 {
         throw idToAliasMap[k];
       }
     }
-    console.log('data2', idToAliasMap);
-
     data.forEach((item) => {
       Object.entries(item).forEach(([key, value]) => {
         const alias = idToAliasMap[key];

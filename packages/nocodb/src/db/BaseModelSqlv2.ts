0a8eaa649f06322abf74fe766f19056f83f41e52
Fendy Heryanto <fendy@nocodb.com>
2025-01-31 02:13:36 +0000

    use isLinksOrLTAR for detection

--- packages/nocodb/src/db/BaseModelSqlv2.ts
+++ packages/nocodb/src/db/BaseModelSqlv2.ts
@@ -4907,8 +4907,7 @@ class BaseModelSqlv2 implements IBaseModelSqlV2 {
       const execQueries: ((trx: Knex.Transaction) => Promise<any>)[] = [];
 
       for (const column of this.model.columns) {
-        if (![UITypes.LinkToAnotherRecord, UITypes.Links].includes(column.uidt))
-          continue;
+        if (!isLinksOrLTAR(column)) continue;
 
         const colOptions =
           await column.getColOptions<LinkToAnotherRecordColumn>(this.context);
@@ -6855,8 +6854,7 @@ class BaseModelSqlv2 implements IBaseModelSqlV2 {
       const base = await this.getSource();
 
       for (const column of this.model.columns) {
-        if (![UITypes.LinkToAnotherRecord, UITypes.Links].includes(column.uidt))
-          continue;
+        if (!isLinksOrLTAR(column)) continue;
 
         const colOptions =
           await column.getColOptions<LinkToAnotherRecordColumn>(this.context);
@@ -6982,8 +6980,7 @@ class BaseModelSqlv2 implements IBaseModelSqlV2 {
       // qb.del();
 
       for (const column of this.model.columns) {
-        if (![UITypes.LinkToAnotherRecord, UITypes.Links].includes(column.uidt))
-          continue;
+        if (!isLinksOrLTAR(column)) continue;
 
         const colOptions =
           await column.getColOptions<LinkToAnotherRecordColumn>(this.context);

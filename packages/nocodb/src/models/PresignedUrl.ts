4c99a5d94d942cc70ac036468465699a11970693
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-02-27 09:34:50 +0000

    chore: remove console.error

--- packages/nocodb/src/models/PresignedUrl.ts
+++ packages/nocodb/src/models/PresignedUrl.ts
@@ -322,7 +322,6 @@ export default class PresignedUrl {
       await Promise.all(promises);
     } catch (e) {
       logger.error('Error signing meta icon image', e);
-      console.error(e);
     }
   }
 }

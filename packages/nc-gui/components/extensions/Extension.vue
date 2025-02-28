cb2b47377b25f52dffa6152ffb20f7beb166a2b1
Amit Joki <amitjoki@gmail.com>
2025-02-28 16:38:03 +0000

    fix: fix data exporter and only show the latest in minimized mode

--- packages/nc-gui/components/extensions/Extension.vue
+++ packages/nc-gui/components/extensions/Extension.vue
@@ -97,6 +97,10 @@ useEventListener('keydown', (e) => {
     fullscreen.value = false
   }
 })
+
+const noExplicitHeightExtensions = ['nc-data-exporter']
+
+const isNoExplicitHeightExtension = computed(() => noExplicitHeightExtensions.includes(extension.value.extensionId))
 </script>
 
 <template>
@@ -113,7 +117,7 @@ useEventListener('keydown', (e) => {
       :style="
         !collapsed
           ? {
-              height: extensionHeight,
+              height: isNoExplicitHeightExtension ? '100%' : extensionHeight,
               minHeight: extensionManifest?.config?.contentMinHeight,
             }
           : {}

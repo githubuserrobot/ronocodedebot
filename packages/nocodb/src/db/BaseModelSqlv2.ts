4ab6ec2556c66ee341bf15eac2f110ba641a316f
Pranav C <pranavxc@gmail.com>
2025-03-08 10:40:46 +0000

    refactor: show filters under filter menu(WIP)

--- packages/nocodb/src/db/BaseModelSqlv2.ts
+++ packages/nocodb/src/db/BaseModelSqlv2.ts
@@ -836,7 +836,7 @@ class BaseModelSqlv2 implements IBaseModelSqlV2 {
       columns,
     );
 
-    const { filtes: filterObj, _ } = extractFilterFromXwhere(
+    const { filters: filterObj } = extractFilterFromXwhere(
       where,
       aliasColObjMap,
     );
@@ -1411,9 +1411,7 @@ class BaseModelSqlv2 implements IBaseModelSqlV2 {
               logical_op: 'and',
             }),
             new Filter({
-              children: {
-                filters: extractFilterFromXwhere(where, aliasColObjMap),
-              },
+              children: extractFilterFromXwhere(where, aliasColObjMap).filters,
               is_group: true,
               logical_op: 'and',
             }),

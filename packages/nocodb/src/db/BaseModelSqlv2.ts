3c607efd9a85cf008a9ba729d97438df459d4ff0
Pranav C <pranavxc@gmail.com>
2025-03-11 06:37:06 +0000

    feat: enable data audit for external db as well(ee)

--- packages/nocodb/src/db/BaseModelSqlv2.ts
+++ packages/nocodb/src/db/BaseModelSqlv2.ts
@@ -11187,9 +11187,7 @@ class BaseModelSqlv2 implements IBaseModelSqlV2 {
   }
 
   async isDataAuditEnabled() {
-    return isDataAuditEnabledFn({
-      isMetaSource: !!(await this.getSource())?.isMeta(),
-    }) as boolean;
+    return isDataAuditEnabledFn() as boolean;
   }
 
   getViewId() {

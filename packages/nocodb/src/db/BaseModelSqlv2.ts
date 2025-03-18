6709ebdc8a1da1f961e8a32e604ee095eed036aa
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-03-18 11:14:11 +0000

    fix: bulk update more than 100 records not working properly

--- packages/nocodb/src/db/BaseModelSqlv2.ts
+++ packages/nocodb/src/db/BaseModelSqlv2.ts
@@ -6259,6 +6259,7 @@ class BaseModelSqlv2 implements IBaseModelSqlV2 {
       const toBeUpdated = [];
       const pkAndData: { pk: any; data: any }[] = [];
       const readChunkSize = 100;
+      let globalIndex = 0;
       for (const [i, d] of updateDatas.entries()) {
         const pkValues = getCompositePkValue(
           this.model.primaryKeys,
@@ -6310,10 +6311,11 @@ class BaseModelSqlv2 implements IBaseModelSqlV2 {
               toBeUpdated.push({ d: data, wherePk });
               updatePkValues.push(
                 getCompositePkValue(this.model.primaryKeys, {
-                  ...prevData[i],
+                  ...prevData[globalIndex],
                   ...data,
                 }),
               );
+              globalIndex++;
             }
           }
         } else {
